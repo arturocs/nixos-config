894f4e0f3d80db4c43265146e840e89a56c795d1
Arturo <arturopalandroid@gmail.com>
2024-06-28 14:33:00 +0200

    Enable zram and auto-optimise-store

--- configuration.nix
+++ configuration.nix
@@ -225,6 +225,9 @@
   # Enable the OpenSSH daemon.
   services.openssh.enable = true;
 
+  zramSwap.enable = true;
+  nix.settings.auto-optimise-store = true;
+
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
   # networking.firewall.allowedUDPPorts = [ ... ];
