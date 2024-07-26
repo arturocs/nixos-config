73dcd95e3473ffbac09f6364d1cdcc9dc1038794
Arturo <arturocs@protonmail.com>
2024-07-26 14:58:13 +0200

    Pin nix-flatpak to 0.4.1

--- flake.nix
+++ flake.nix
@@ -16,7 +16,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
       inputs.home-manager.follows = "home-manager";
     };
-    nix-flatpak.url = "github:gmodena/nix-flatpak"; # unstable branch. Use github:gmodena/nix-flatpak/?ref=<tag> to pin releases.
+    nix-flatpak.url = "github:gmodena/nix-flatpak?ref=v0.4.1";
     autofirma-nix.url = "github:nilp0inter/autofirma-nix/release-24.05";
   };
 
