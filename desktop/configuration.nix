4daba40b4682660981567b7ebfa64aa58de05cd5
Arturo <arturocs@protonmail.com>
2024-09-06 14:28:29 +0200

    Enable bluetooth

--- desktop/configuration.nix
+++ desktop/configuration.nix
@@ -4,6 +4,8 @@
   pkgs,
   ...
 }: {
+  hardware.bluetooth.enable = true; 
+  hardware.bluetooth.powerOnBoot = false;
   boot.loader.systemd-boot.enable = true;
   boot.loader.systemd-boot.configurationLimit = 3;
   boot.loader.efi.canTouchEfiVariables = true;
