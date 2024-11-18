f9a86ef3c2db28cd389d10ef22d78b202c763cb4
Arturo <arturocs@protonmail.com>
2024-11-18 19:40:02 +0100

    Use Tmpfs for builds on desktop

--- desktop/configuration.nix
+++ desktop/configuration.nix
@@ -11,6 +11,7 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.systemd-boot.configurationLimit = 3;
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.tmp.useTmpfs = true;
   networking.hostName = "nixos"; # Define your hostname.
   # Enable OpenGL
   hardware.opengl = {
