73e68f63ad31012f959f8b257249e7e22dc52be4
Arturo <arturocs@protonmail.com>
2024-12-07 14:58:15 +0100

    Install nix-ide

--- unstable_packages.nix
+++ unstable_packages.nix
@@ -36,7 +36,7 @@
     (vscode-with-extensions.override
       {
         vscodeExtensions = with vscode-extensions; [
-          bbenoist.nix
+          jnoortheen.nix-ide
           ms-python.python
           ms-azuretools.vscode-docker
           ms-vscode-remote.remote-ssh
