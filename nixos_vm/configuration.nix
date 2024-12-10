cad9635dabf72cbeb457fa3cbecb17ef096899bf
Arturo <arturocs@protonmail.com>
2024-12-10 16:47:42 +0100

    Create rebuild and update aliases

--- nixos_vm/configuration.nix
+++ nixos_vm/configuration.nix
@@ -5,4 +5,9 @@
   boot.loader.grub.useOSProber = true;
   networking.hostName = "nixos_vm"; # Define your hostname.
   virtualisation.virtualbox.guest.enable = true;
+  environment.shellAliases = {
+    update = "cd /home/arturo/nixos-config && nix flake update && nh os switch -H nixos_vm .";
+    rebuild = "cd /home/arturo/nixos-config && nh os switch -H nixos_vm .";
+  };
+
 }
