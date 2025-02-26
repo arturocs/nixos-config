48914fc4a924ed35366c60d69b503c606220670f
Arturo <arturocs@protonmail.com>
2025-02-26 12:46:52 +0100

    Install nvidia drivers from nixpkgs-unstable

--- desktop/configuration.nix
+++ desktop/configuration.nix
@@ -1,6 +1,7 @@
 {
   config,
   pkgs,
+  pkgs-unstable,
   ...
 }: {
   hardware.bluetooth.enable = true;
@@ -50,12 +51,6 @@
     nvidiaSettings = true;
 
     # Optionally, you may need to select the appropriate driver version for your specific GPU.
-    package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
-      version = "570.86.16"; # use new 570 drivers
-      sha256_64bit = "sha256-RWPqS7ZUJH9JEAWlfHLGdqrNlavhaR1xMyzs8lJhy9U=";
-      openSha256 = "sha256-DuVNA63+pJ8IB7Tw2gM4HbwlOh1bcDg2AN2mbEU9VPE=";
-      settingsSha256 = "sha256-9rtqh64TyhDF5fFAYiWl3oDHzKJqyOW3abpcf2iNRT8=";
-      usePersistenced = false;
-    };
+    package = pkgs-unstable.linuxKernel.packages.linux_6_13.nvidia_x11_latest;
   };
 }
