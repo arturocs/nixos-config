684a7db7d583e68c0bc9e95366e7916733d4bf7e
Arturo <arturocs@protonmail.com>
2025-03-04 19:44:01 +0100

    Add nofail option to HDD mounts

--- desktop/hardware-configuration.nix
+++ desktop/hardware-configuration.nix
@@ -31,22 +31,25 @@
   fileSystems."/hdd" = {
     device = "/dev/sda2";
     fsType = "ntfs3";
-    options = ["nosuid" "nodev" "uid=1000" "gid=100" "iocharset=utf8" "uhelper=udisks2"];
+    options = ["nosuid" "nodev" "uid=1000" "gid=100" "iocharset=utf8" "uhelper=udisks2" "nofail"];
   };
 
   fileSystems."/home/arturo/.local/share/dolphin-emu/Load/Textures/Metroid Prime 1 HD" = {
     device = "/hdd/ROMS/Dolphin textures/Metroid_Prime_1.sfs";
     fsType = "squashfs";
+    options = ["nofail"];
   };
 
   fileSystems."/home/arturo/.local/share/dolphin-emu/Load/Textures/Metroid Prime 2 HD" = {
     device = "/hdd/ROMS/Dolphin textures/Metroid_Prime_2.sfs";
     fsType = "squashfs";
+    options = ["nofail"];
   };
 
   fileSystems."/home/arturo/.local/share/dolphin-emu/Load/Textures/Metroid Prime 3 HD" = {
     device = "/hdd/ROMS/Dolphin textures/Metroid_Prime_3.sfs";
     fsType = "squashfs";
+    options = ["nofail"];
   };
 
   swapDevices = [];
