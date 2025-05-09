ba88336e8209476769ad2e8c6a289cc3bd2bd18a
Arturo <arturocs@protonmail.com>
2025-05-09 10:04:04 +0200

    Remove podman from nixosvm

--- nixosvm/configuration.nix
+++ nixosvm/configuration.nix
@@ -9,20 +9,4 @@
     #!/bin/sh
     ln -sf /nix/flatpak /var/lib/flatpak
   '';
-  virtualisation.containers.enable = true;
-  virtualisation = {
-    podman = {
-      enable = true;
-      # Create a `docker` alias for podman, to use it as a drop-in replacement
-      dockerCompat = true;
-      # Required for containers under podman-compose to be able to talk to each other.
-      defaultNetwork.settings.dns_enabled = true;
-    };
-  };
-
-  # Useful other development tools
-  environment.systemPackages = with pkgs; [
-    docker-compose # start group of containers for dev
-    #podman-compose # start group of containers for dev
-  ];
 }
