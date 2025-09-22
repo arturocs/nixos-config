ba9d6f607c32a6f7e64d35230d2013a94bbe5acc
Arturo <arturocs@protonmail.com>
2025-09-22 11:10:42 +0000

    Remove flake utils

--- flake.nix
+++ flake.nix
@@ -16,7 +16,6 @@
     nixpkgs-unstable.url = "git+https://github.com/NixOS/nixpkgs.git?ref=nixos-unstable";
     home-manager.url = "git+https://github.com/nix-community/home-manager.git?ref=release-25.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
-    flake-utils.url = "git+https://github.com/numtide/flake-utils.git";
     nur = {
       url = "git+https://github.com/nix-community/NUR.git";
       inputs.nixpkgs.follows = "nixpkgs";
