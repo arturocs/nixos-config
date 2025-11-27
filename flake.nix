fcfaa267265d58fd8015d9739892c7d990eee3cf
Arturo <arturocs@protonmail.com>
2025-11-27 14:53:13 +0000

    Use nixos-25.11 branch instead of release-25.11

--- flake.nix
+++ flake.nix
@@ -12,7 +12,7 @@
   };
 
   inputs = {
-    nixpkgs.url = "git+https://github.com/NixOS/nixpkgs.git?ref=release-25.11";
+    nixpkgs.url = "git+https://github.com/NixOS/nixpkgs.git?ref=nixos-25.11";
     nixpkgs-unstable.url = "git+https://github.com/NixOS/nixpkgs.git?ref=nixpkgs-unstable";
     home-manager.url = "git+https://github.com/nix-community/home-manager.git?ref=release-25.11";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
