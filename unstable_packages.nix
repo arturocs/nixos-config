de7367502a4ee70785f0958bae87a412c92e0940
Arturo <arturocs@protonmail.com>
2025-11-30 17:26:57 +0000

    Add orca-slicer launch command

--- unstable_packages.nix
+++ unstable_packages.nix
@@ -19,6 +19,8 @@
     cura-appimage
     llama-cpp
     hydralauncher
+    # Launch command on wayland
+    # env __GLX_VENDOR_LIBRARY_NAME=mesa __EGL_VENDOR_LIBRARY_FILENAMES=/run/opengl-driver/share/glvnd/egl_vendor.d/50_mesa.json MESA_LOADER_DRIVER_OVERRIDE=zink GALLIUM_DRIVER=zink WEBKIT_DISABLE_DMABUF_RENDERER=1 orca-slicer
     orca-slicer
     (retroarch.withCores (
       cores:
