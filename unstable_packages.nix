30bea3a38b224580a8ccc478a9c6f43fc26228b0
Arturo <arturocs@protonmail.com>
2025-12-18 07:40:51 +0000

    Install antigravity

--- unstable_packages.nix
+++ unstable_packages.nix
@@ -22,6 +22,7 @@
     # Launch command on wayland
     # env __GLX_VENDOR_LIBRARY_NAME=mesa __EGL_VENDOR_LIBRARY_FILENAMES=/run/opengl-driver/share/glvnd/egl_vendor.d/50_mesa.json MESA_LOADER_DRIVER_OVERRIDE=zink GALLIUM_DRIVER=zink WEBKIT_DISABLE_DMABUF_RENDERER=1 orca-slicer
     orca-slicer
+    antigravity-fhs
     (retroarch.withCores (
       cores:
         with libretro; [
