e9ff014c5d14ecc44a67e5d7483ab5ec3bcd8794
Arturo <arturocs@protonmail.com>
2026-01-29 00:58:36 +0100

    Move nix cache to bcachefs in nixosvm

--- nixosvm/configuration.nix
+++ nixosvm/configuration.nix
@@ -5,8 +5,9 @@
   boot.loader.grub.useOSProber = true;
   networking.hostName = "nixosvm"; # Define your hostname.
   virtualisation.virtualbox.guest.enable = true;
-  system.activationScripts.createFlatpakSymlink.text = ''
+  system.activationScripts.createSymlinks.text = ''
     #!/bin/sh
     ln -sf /nix/flatpak /var/lib/flatpak
+    ln -sf /nix/nix_cache /home/arturo/.cache/nix/
   '';
 }
