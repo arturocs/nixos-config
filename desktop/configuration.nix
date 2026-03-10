7a6d1ae9ed9c2559de8e98683272d00212332eb2
Arturo <arturocs@protonmail.com>
2026-03-10 09:43:50 +0000

    Use nvidia driver 590.48.01

--- desktop/configuration.nix
+++ desktop/configuration.nix
@@ -71,15 +71,15 @@
     nvidiaSettings = true;
 
     # Optionally, you may need to select the appropriate driver version for your specific GPU.
+
     #package = pkgs.linuxPackages_latest.nvidia_x11;
-    package = pkgs-unstable.linuxPackages_latest.nvidia_x11;
-    # package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
-    #   version = "580.95.05";
-    #   sha256_64bit = "sha256-hJ7w746EK5gGss3p8RwTA9VPGpp2lGfk5dlhsv4Rgqc=";
-    #   sha256_aarch64 = "sha256-zLRCbpiik2fGDa+d80wqV3ZV1U1b4lRjzNQJsLLlICk=";
-    #   openSha256 = "sha256-RFwDGQOi9jVngVONCOB5m/IYKZIeGEle7h0+0yGnBEI=";
-    #   settingsSha256 = "sha256-F2wmUEaRrpR1Vz0TQSwVK4Fv13f3J9NJLtBe4UP2f14=";
-    #   persistencedSha256 = "sha256-QCwxXQfG/Pa7jSTBB0xD3lsIofcerAWWAHKvWjWGQtg=";
-    # };
+    #package = pkgs-unstable.linuxPackages_latest.nvidia_x11;
+    package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
+      version = "590.48.01";
+      sha256_64bit = "sha256-ueL4BpN4FDHMh/TNKRCeEz3Oy1ClDWto1LO/LWlr1ok=";
+      openSha256 = "sha256-hECHfguzwduEfPo5pCDjWE/MjtRDhINVr4b1awFdP44=";
+      settingsSha256 = "sha256-4SfCWp3swUp+x+4cuIZ7SA5H7/NoizqgPJ6S9fm90fA=";
+      persistencedSha256 = "";
+    };
   };
 }
