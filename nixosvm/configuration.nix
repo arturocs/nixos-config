4e42f37afcf3765b0a9cf1cd67cdc262f123bbe9
Arturo <arturocs@protonmail.com>
2026-05-25 12:54:38 +0200

    Add bcachefs-tools to nixosvm configuration

--- nixosvm/configuration.nix
+++ nixosvm/configuration.nix
@@ -5,6 +5,9 @@
   boot.loader.grub.useOSProber = true;
   networking.hostName = "nixosvm"; # Define your hostname.
   virtualisation.virtualbox.guest.enable = true;
+  environment.systemPackages = [
+    pkgs.bcachefs-tools
+  ];
   system.activationScripts.createSymlinks.text = ''
     #!/bin/sh
     ln -sf /nix/flatpak /var/lib/flatpak
